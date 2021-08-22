0357cd713fd820b1ccb0a99726298362a3ca400a
MrMirror21 <mylittlesentry@gmail.com>
2021-08-22 16:15:45 +0900

    divide LandingPage/Home for Guest/User

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 import Welcome from '../components/Welcome';
+import Header from '../components/Header';
+import { Redirect } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    display: inline;
+`;
 
 const Home = () => {
-    return (
+    const [isUser, setIsUser] = useState(true);
+    return isUser ? 
         <Wrapper>
+            <Header/>
             <Welcome/>
         </Wrapper>
-    );
+    : <Redirect to="/LandingPage"/> ;
 };
 
 export default Home;
