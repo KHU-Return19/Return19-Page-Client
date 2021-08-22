dbdbe2f7984b25d9e7a1e02caebb20ec9dc2d65a
MrMirror21 <mylittlesentry@gmail.com>
2021-08-22 18:06:26 +0900

    feat:Divdie guest and user by accesstoken

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,20 +3,19 @@ import styled from 'styled-components';
 import Welcome from '../components/Welcome';
 import Header from '../components/Header';
 import { Redirect } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 
 const Wrapper = styled.div`
     display: inline;
 `;
 
 const Home = () => {
-    const [isUser, setIsUser] = useState(true);
-    return isUser ? 
-        <Wrapper>
-            <Header/>
-            <Welcome/>
-        </Wrapper>
-    : <Redirect to="/LandingPage"/> ;
+    const token = localStorage.getItem('accessToken');
+    return token ? 
+    <Wrapper>
+        <Header/>
+        <Welcome/>
+    </Wrapper>
+    : <Redirect to="/LandingPage"/>
 };
 
 export default Home;
