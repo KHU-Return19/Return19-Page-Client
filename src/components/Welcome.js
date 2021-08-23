d7bdf9cdd32e4c83260ee7cbdd892c93041828d4
MrMirror21 <mylittlesentry@gmail.com>
2021-08-23 19:14:53 +0900

    Update welcome text

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,10 @@ import styled from 'styled-components';
 const Wrapper = styled.div``;
 
 const Welcome = () => {
-    return <Wrapper>Welcome to Home!</Wrapper>
+    return <Wrapper>
+        <h2>As long as you live and breathe.</h2>
+        <h2>Move forward in the name of Kyung-hee.</h2> 
+    </Wrapper>
 }
 
 export default Welcome;
