d4eacd52734369ff1c4fbb1755d0c0dd1b6305be
MrMirror21 <mylittlesentry@gmail.com>
2021-08-23 19:14:08 +0900

    feat: add SignUpPage
    - guest will led to signup page by the button of landing page.

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import LandingPage from './pages/LandingPage';
+import SignUpPage from './pages/SignUpPage';
 
 
 const Router = () => {
@@ -9,6 +10,7 @@ const Router = () => {
         <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/LandingPage" component={LandingPage} />
+            <Route exact path="/SignUpPage" component={SignUpPage} />
             <Route path="/">404 not found</Route>
         </Switch>
     );
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Welcome from '../components/Welcome';
+import Button from '../components/Button';
+
+const Wrapper = styled.div``;
 
 const LandingPage = () => {
     return (
-        <div>
-            This is LandingPage
-        </div>
+        <Wrapper>
+            <Welcome />
+            <Link to="/SignUpPage">
+                <Button color="lightgrey">SIGN UP</Button>
+            </Link>
+        </Wrapper>
     )
 }
 
