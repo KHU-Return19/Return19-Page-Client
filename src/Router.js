1a9c2850af7776874850728c70110f68a61848f1
MrMirror21 <mylittlesentry@gmail.com>
2021-08-31 18:24:14 +0900

    resolve conflicts

--- src/Router.js
+++ src/Router.js
@@ -3,15 +3,21 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import LandingPage from './pages/LandingPage';
 import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import MemberPage from './pages/MemberPage';
+import ProfilePage from './pages/ProfilePage';
+import Auth from "./hoc/auth"
 import CalendarPage from './pages/CalendarPage';
-
 
 const Router = () => {
     return (
         <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/LandingPage" component={LandingPage} />
-            <Route exact path="/SignUpPage" component={SignUpPage} />
+            <Route exact path="/" component={Auth(Home, true)} />
+            <Route exact path="/LandingPage" component={Auth(LandingPage, false)} />
+            <Route exact path="/signup" component={Auth(SignUpPage, false)} />
+            <Route exact path="/login" component={Auth(LoginPage, false)} />
+            <Route exact path="/members" component={Auth(MemberPage,true)} />
+            <Route exact path="/profile" component={Auth(ProfilePage,true)} />
             <Route exact path="/CalendarPage" component={CalendarPage} />
             <Route path="/">404 not found</Route>
         </Switch>
